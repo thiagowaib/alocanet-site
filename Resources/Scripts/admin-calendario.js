e2367f65f48272a4092048931fa2f40b5127e8aa
thiagowaib <thiagowaib@gmail.com>
2022-10-13 11:43:26 -0300

    [New] Instanciação do Calendário na página de agendamentos admiN

---
+++ Resources/Scripts/admin-calendario.js
@@ -0,0 +1,11 @@
+/**
+ * Script utilizado no funcionamento do
+ * calendáro da página de agendamentos (Admin)
+ */
+const calendar = new VanillaCalendar("#calendar", {
+    settings: {
+        lang: 'pt-br',
+    }
+})
+calendar.init()
+
