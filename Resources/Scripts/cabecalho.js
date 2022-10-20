5ef2b707da9fd49f6891a14126ef35559332b979
thiagowaib <thiagowaib@gmail.com>
2022-10-20 18:50:11 -0300

    [New] Redirecionamento caso usuário pule para arquivo html especifico sem login

--- Resources/Scripts/cabecalho.js
+++ Resources/Scripts/cabecalho.js
@@ -3,6 +3,32 @@
  * cabeçalho no painel administrativo
  */
 
+// Verifica se há o JWT, se não envia o usuário para tela de login
+try{
+    const JWT = document.cookie
+                .split("; ")
+                .find(tag => tag.startsWith("JWT="))
+                .split("=")[1]
+    fetch("https://alocanet-servidor.glitch.me/authJWT", {
+        method: "GET",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            "auth": `${JWT}`
+        },
+        cache: "no-store"
+    })
+    .then((res) => {
+        if(res.status.toString()[0] === "4")
+            window.location.href = "./index.html"
+    })
+    .catch((err) => {
+        console.error(err)
+        window.location.href = "./index.html"
+    })
+}catch{
+    window.location.href = "./index.html"
+}
+
 // EventListener para realizar Logout
 document.getElementById("botao-logout").addEventListener("click", () => {
     document.cookie = "JWT="                     // -> Apaga o JWT dos Cookies
@@ -20,3 +46,4 @@ document.getElementById("botao-fechar-menu").addEventListener("click", () => {
     const menu = document.getElementById("menu-navegacao")
     menu.style.display = "none"
 })
+
